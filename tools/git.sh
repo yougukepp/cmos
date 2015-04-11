dd5e963a2e992d153a5a9e248baf7119ef83cb4e
PengPeng <pengpeng@fiberhome.com>
2015-04-11 20:27:02 +0800

    完善git.sh记录

--- tools/git.sh
+++ tools/git.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-# 推送 tag到远程
+# 推送tag到远程
 #git push --tag
+# 获取远程tag
+#git fetch origin tag <tagname>
 
 #删除远程分支
 #git push origin --delete <branchName>
-
 #推送test到远程分支
 #git push origin test:test 
