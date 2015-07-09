2d571722f05d7daffb3487d9a5aeda2ae7f2e932
pengpeng <pengpeng@fiberhome.com>
2015-07-09 15:47:44 +0800

    清理垃圾文件

--- tools/clean.sh
+++ tools/clean.sh
@@ -4,8 +4,3 @@ rm -rf ./prj/lst/*
 rm -rf ./prj/obj/*
 rm -rf ./prj/cmos.uvguix*
 
-rm -rf ./app/flyer/eMD6.12/prj/lst/*
-rm -rf ./app/flyer/eMD6.12/prj/obj/*
-rm -rf ./app/flyer/eMD6.12/prj/md.uvguix*
-
-
