9c856ad10cc80b6151476c4eadc5af63be45910c
pengpeng <pengpeng@fiberhome.com>
2015-07-09 10:41:12 +0800

    更新工具脚本

--- tools/clean.sh
+++ tools/clean.sh
@@ -2,5 +2,10 @@
 
 rm -rf ./prj/lst/*
 rm -rf ./prj/obj/*
-rm -rf ./prj/cmos.uvguix.Administrator
+rm -rf ./prj/cmos.uvguix*
+
+rm -rf ./app/flyer/eMD6.12/prj/lst/*
+rm -rf ./app/flyer/eMD6.12/prj/obj/*
+rm -rf ./app/flyer/eMD6.12/prj/md.uvguix*
+
 
