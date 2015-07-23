8c418456bd8c2f5bc76ebd1512b22159cb739826
pengpeng <pengpeng@fiberhome.com>
2015-07-23 16:07:56 +0800

    将obj实现为类完成

--- tools/flyer_controller/fc_flyer.py
+++ tools/flyer_controller/fc_flyer.py
@@ -19,12 +19,25 @@ class FCFlyer():
         self.mConfigs = None
         self.mObjs = None
 
-        self.ParserJson(jsonName)
+        self.Parse(jsonName)
         self.MakeFlyer()
         self.ApplyConfigs() 
-        #print(self.mObjs["Propeller"])
-        self.DataToArray()
         self.ModelNESToGLXYZ()
+        self.DataToArray()
+
+    def Parse(self, jsonName):
+        jsonPaser = FCJsonPaser(jsonName)
+        data = jsonPaser.ToDict()
+
+        # 配置
+        self.mConfigs = {}
+        self.mConfigs = data['Configs']
+
+        # obj对象
+        self.mObjs = {}
+        objsDict = data['Objs']
+        for objName in objsDict:
+            self.mObjs[objName] = FCObj(objsDict, objName)
 
     def PaintGL(self):
         objsDict = self.mObjs
@@ -34,26 +47,67 @@ class FCFlyer():
 
         # 逐个obj
         for objName in objsDict:
-            self.AObjPaintGL(objName)
+            objsDict[objName].PaintGL()
 
         GL.glDisableClientState( GL.GL_COLOR_ARRAY )
         GL.glDisableClientState( GL.GL_VERTEX_ARRAY ) 
 
-    def AObjPaintGL(self, objName):
-        # 图形数据
-        objDict = self.mObjs[objName]
+    def ApplyConfigs(self):
+        scale = self.mConfigs["Scale"]
+        alpha = self.mConfigs["Alpha"]
 
-        colorsData = objDict['Colors']
-        verticesData = objDict['Vertices']
-        indicesData = objDict['Indices']
-        drawType = objDict['DrawType']
-        lineWidth = objDict['LineWidth']
+        for obj in self.mObjs:
+            #print(obj)
+            self.mObjs[obj].UpdateScaleOrAlpha('Vertices', scale)
+            self.mObjs[obj].UpdateScaleOrAlpha('Colors', alpha)
 
+    def DataToArray(self):
+        for obj in self.mObjs:
+            self.mObjs[obj].DataToArray()
+
+    def MakeFlyer(self):
+        """
+        平移出一个部分
+        旋转出三个对称的部分
+        """
+        config = self.mConfigs
+        objsDict = self.mObjs
+        armLength = config['ArmLength']
+        angleList = config['AngleList']
+
+        # print(armLength)
+        # 平移 所有顶点y-armLength
+        for obj in self.mObjs:
+            #print(objName)
+            self.mObjs[obj].Translate(0, armLength, 0)
+
+        #print(angleList)
+        # 旋转
+
+    # 转换到 模型:北N东E天S => GL:XYZ
+    def ModelNESToGLXYZ(self):
+        pass
+
+class FCObj():
+    def __init__(self, jsonDict, objName):
         #print(objName)
+        self.mName = objName
+        self.mData = jsonDict[objName]
+
+    def PaintGL(self):
+        # 图形数据
+        data = self.mData
+
+        colorsData = data['Colors']
+        verticesData = data['Vertices']
+        indicesData = data['Indices']
+        drawType = data['DrawType']
+        lineWidth = data['LineWidth']
+
+        #print(self.mName)
         #print(verticesData)
         #print(colorsData)
         #print(indicesData)
-
         if 0 == len(indicesData) or 0 == len(colorsData) or 0 == len(verticesData):
             #print('%s无数据可绘制' % objName)
             return
@@ -83,149 +137,73 @@ class FCFlyer():
         else:
             print('绘制类型%s错误.' % DrawType)
 
-    def ParserJson(self, jsonName):
-        self.mConfigs = {}
-        self.mObjs = {}
-
-        jsonPaser = FCJsonPaser(jsonName)
-
-        data = jsonPaser.ToDict()
-
-        self.mConfigs = data['Configs']
-        self.mObjs = data['Objs']
-
-    def ApplyConfigs(self):
-        scale = self.mConfigs["Scale"]
-        alpha = self.mConfigs["Alpha"]
-
-        self.UpdateScale(scale)
-        self.UpdateAlpha(alpha)
-
-    def UpdateScale(self, scale):
-        self.UpdateAAttribute(scale, 'Vertices')
-
-    def UpdateAlpha(self, alpha):
-        self.UpdateAAttribute(alpha, 'Colors')
-
-    def UpdateAAttribute(self, para, attributeName):
-        """
-        更新某一个参数 的配置
-
-        para 参数值 :
-        scale 缩放
-        alpha 透明度
-
-        attributeName 文件属性名字:
-        Vertices 顶点
-        Colors 颜色
-        """
-        objsDict = self.mObjs
-
-        # 逐个obj
-        for objName in objsDict:
-            #print(objName)
-            objData = objsDict[objName]
-            oldData = objData[attributeName]
-            newData = self.UpdateAConfig(oldData, para)
-            self.mObjs[objName][attributeName] = newData
-
-    def UpdateAConfig(self, oldData, config): 
+    # 齐次坐标 加入到最后一维
+    def UpdateScaleOrAlpha(self, keyStr, scaleOrAlpha):
         i = 0
+        oldData = self.mData[keyStr]
         newData = []
-        for aData in oldData:
-            if 0 == i%3 and 0 != i:
-                newData.append(config)
+        #print("%s.%s:" % (self.mName, keyStr))
+        #print(oldData)
+        for aData in self.mData[keyStr]:
             newData.append(aData)
+            if 0 == (i+1)%3:
+                newData.append(scaleOrAlpha)
             i += 1
-        newData.append(config) 
-        #print('old:')
-        #print(oldData) 
-        #print('new:')
         #print(newData)
+        self.mData[keyStr] = newData
 
-        return newData
-
-    def DataToArray(self):
-        objsDict = self.mObjs
-
-        # 逐个obj
-        for objName in objsDict:
-            #print(objName)
-            objData = objsDict[objName]
-            for attributeName in objData: 
-                listData = objData[attributeName]
-                if 'Vertices' == attributeName or 'Colors' == attributeName:
-                    arrayData = array.array('f', listData)
-                elif 'Indices' == attributeName:
-                    arrayData = []
-                    for aIndiceItem in listData:
-                        aIndiceArrayItem = array.array('B', aIndiceItem)
-                        arrayData.append(aIndiceArrayItem)
-                    arrayData.append([]) # 保证一维也对
-                else: # 其他属性不转换
-                    continue
-                self.mObjs[objName][attributeName] = arrayData
-                #print(self.mObjs[objName][attributeName])
-
-    def MakeFlyer(self):
+    def Translate(self, n, e, s):
         """
-        平移出一个部分
-        旋转出三个对称的部分
+        平移
+        n 北
+        e 东
+        s 天
         """
-        config = self.mConfigs
-        objsDict = self.mObjs
-        armLength = config['ArmLength']
-        angleList = config['AngleList']
+        #print("%s translate:" % self.mName) 
+        data = self.mData['Vertices']
+        i = 0
+        iMax = len(data) 
+        #print(data)
+        for i in range(0, iMax):
+            #北
+            if 0 == i % 3:
+                self.mData['Vertices'][i] = data[i] + n
+            #东
+            if 0 == (i+2) % 3:
+                self.mData['Vertices'][i] = data[i] + e
+            #天
+            if 0 == (i+1) % 3:
+                self.mData['Vertices'][i] = data[i] + s
+        #print(self.mData['Vertices'])
 
-        # print(armLength)
-        # 平移 所有顶点y-armLength
-        for objName in objsDict:
-            #print(objName)
-            objData = objsDict[objName]
-            for attributeName in objData:
-                if 'Vertices' == attributeName:
-                    vertices = objData[attributeName]
-                    iMax = len(vertices)
-                    #print(vertices)
-                    #print(iMax)
-                    for i in range(0, iMax):
-                        if 0 == (i+2)%3:
-                            value = vertices[i]
-                            self.mObjs[objName][attributeName][i] = value - armLength
-                else:
-                    continue
-
-        print(angleList)
-        # 旋转
-
-    # 用于重构代码的遍历
-    def WalkObjs(self, objsDict, funcToObj):
-        """
-        遍历对象实施操作
-        """
-        for obj in objsDict:
-            funcToObj(obj)
-
-    def WalkAttribute(self, objDict, funcToAttribute):
-        pass
-
-    # 转换到 模型:北N东E天S => GL:XYZ
-    def ModelNESToGLXYZ(self):
-        objsDict = self.mObjs
-
-        # 遍历所有对象
-        for objName in objsDict:
-            #print(objName)
-            objData = objsDict[objName]
-            for attributeName in objData:
-                pass
-
-class FCObj():
-    def __init__(self, jsonName, objName):
-        pass
-
+    def DataToArray(self): 
+        for attributeName in self.mData: 
+            #print("%s.%s:" % (self.mName, attributeName)) 
+            if 'Vertices' == attributeName or 'Colors' == attributeName:
+                """
+                顶点和色彩转换为浮点数组
+                """ 
+                oldData = self.mData[attributeName]
+                newData = array.array('f', oldData)
+            elif 'Indices' == attributeName:
+                """
+                索引转换为Byte数组
+                """ 
+                oldData = self.mData[attributeName]
+                newData = []
+                for aIndiceItem in oldData:
+                    aIndiceArrayItem = array.array('B', aIndiceItem)
+                    newData.append(aIndiceArrayItem)
+                newData.append([]) # 保证一维也对 
+            else: 
+                """其他属性不转换"""
+                continue 
+            #print(oldData)
+            #print(newData)
+            self.mData[attributeName] = newData
 
 if __name__ == '__main__': 
     flyer = FCFlyer()
     print('测试通过')
+
 
