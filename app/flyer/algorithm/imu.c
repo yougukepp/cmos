0146ec5ef16525d44f93672827da71a7ec049e6d
PengPeng <pengpeng@fiberhome.com>
2015-08-19 16:51:53 +0800

    完善注释

--- app/flyer/algorithm/imu.c
+++ app/flyer/algorithm/imu.c
@@ -284,6 +284,7 @@ int imu_get_attitude(float *attitude)
     q3 = s_quaternion[3];
     quaternion_unlock();
 
+    /* TODO: 文档或者公式有错 */
     yaw   = atan2(2*(q1*q2 - q0*q3), 2*(q0*q0 + q1*q1) - 1);
     roll  = -asin(2*(q1*q3 + q0*q2));
     pitch = atan2(2*(q2*q3 - q0*q1), 2*(q0*q0 + q3*q3) - 1);
